41371a253fc34873d2a4fa53eb862c78ebdf9a09
Dmitriy Kozak <dmitrij.kozak.1997@bk.ru>
2023-09-27 20:18:56 +0400

    update

--- src/test/java/CardOrderFormTest.java
+++ src/test/java/CardOrderFormTest.java
@@ -43,7 +43,7 @@ public class CardOrderFormTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78005555555");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
 
     }
